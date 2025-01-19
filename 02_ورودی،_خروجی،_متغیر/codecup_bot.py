2f6c9886f63c59f3f292941377cd5182f2870ecb
Parham Porkhial <p.porkhial@gmail.com>
2025-01-20 00:04:31 +0330

    Solving the extra practices

---
+++ 02_ورودی،_خروجی،_متغیر/codecup_bot.py
@@ -0,0 +1,7 @@
+'''
+Input(n): just a natural number which indicates the number of CodeCup.
+Output: Hello CodeCup n!
+'''
+
+n = int(input())
+print(f"Hello CodeCup {n}!")
---
+++ 02_ورودی،_خروجی،_متغیر/foorosh_Akbar.py
@@ -0,0 +1,11 @@
+'''
+از جمله کسانی که به آرمان قبیله‌ی «گاوچران‌های بی‌ادعا» خیانت کرد، شخصی به نام «اسماعیل پشم‌زاده» یا به اختصار «پشمال‌آقا» است. پشمال‌آقا با پرورش گوسفند به جای گاو و پشت‌کردن به آداب و رسوم قبیله، از جانب هم‌قبیله‌ای‌هایش طرد شده و هم‌اکنون در صحرای اطرافِ قبیله در حال گوسفندچرانی است.
+
+n = tedad gusfand ha
+k = killo pashm e har gusfand(ashari e)
+p = gheimat har killo pashm
+output --> chegahd pul mitune dar biare
+'''
+n, k, p = map(float, input().split())
+money = int(n*p*k)
+print(money)
