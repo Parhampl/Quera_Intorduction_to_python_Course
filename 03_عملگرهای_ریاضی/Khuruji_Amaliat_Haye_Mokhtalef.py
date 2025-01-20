d5ea339476df2bba2528b8d0e894e74c8997de23
Parham Porkhial <p.porkhial@gmail.com>
2025-01-20 03:30:37 +0330

    Tamrin Outuput haye mookhtalef

---
+++ 03_عملگرهای_ریاضی/Khuruji_Amaliat_Haye_Mokhtalef.py
@@ -0,0 +1,17 @@
+'''Be tore koli har chizi ke dar chize dg i + - * ** berese va yeki az adad ashari bashe 
+hata ba ashar 0(20.0) khuruji ro ashari ya float mikone.
+TAGHSIM(/) HAMISHE KHURUJI FLOAT DARE
+MAGAR AZ TAGHSIM SAHIH(//) ESTEFADE KONIM
+'''
+a = 1 + 2
+b = 5.0 - 3
+print('a =', a, 'b =', b)  # a = 3 b = 2.0
+c = a * 2
+d = c / 2  # d = 3.0
+e = c // 2  # e = 3
+print('a =', a, 'c =', c, 'd =', d, 'e =', e)  # a = 3 c = 6 d = 3.0 e = 3
+f = a ** b
+print('f = ', f)  # f = 9.0
+s = t = '12'
+print('s + t =', s + t, 's * 3 =', s * 3,
+      '4 * t =', 4 * t)  # 1212    121212  12121212
