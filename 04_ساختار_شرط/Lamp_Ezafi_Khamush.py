9457022001be8013a07c53eab1362d21af6de5c0
Parham Porkhial <p.porkhial@gmail.com>
2025-02-02 16:02:14 +0330

    Solve Lamp ezafi khamush with 2 approaches

---
+++ 04_ساختار_شرط/Lamp_Ezafi_Khamush.py
@@ -0,0 +1,13 @@
+House_Area = input()
+print(len(House_Area))
+
+# #Alternative Way
+# House_Area = int(input())
+# if House_Area <10:
+#     print(1)
+# elif House_Area <100:
+#     print(2)
+# elif House_Area <1000:
+#     print(3)
+# else:
+#     print(4)
