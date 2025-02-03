0ba6e650d024989dbe5e2b2a21880f709c7f58a7
Parham Porkhial <p.porkhial@gmail.com>
2025-02-03 18:47:33 +0330

    Modifying Triangle or Not Problem

--- 04_ساختار_شرط/Triangle_Or_Not.py
+++ 04_ساختار_شرط/Triangle_Or_Not.py
@@ -1,5 +1,7 @@
 # Three digit can be sides of a triangle when the sum of each two pair is greater than the other sidea, b, c = map(int, input().split())
 
+a, b, c = map(int, input().split())
+
 if a+b > c and a+c > b and b+c > a:
     print("Bale")
 else:
